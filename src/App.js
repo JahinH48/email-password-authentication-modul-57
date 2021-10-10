81e6b6140018155aecbfd018b37c032569230677
unknown <jahinhasan48@gmail.com>
2021-10-10 11:11:47 +0600

    jisan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {getAuth , signInWithPopup, createUserWithEmailAndPassword  , GoogleAuthProvider ,sendEmailVerification ,sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+import initional from './firebase/firebase.init';
+
+initional()
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const[email , setEmail] = useState('');
+  const [password , setPassword ] =useState('');
+  const [error , setError] = useState('');
+  const auth = getAuth();
+  const [isLogin , setisLogin ] = useState(false);
+
+  const googleLoginClick =() =>{
+    
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+
+  }
+const emailhendelar =e =>{
+  setEmail(e.target.value);
+}
+
+const passwordhendel =e =>{
+  setPassword(e.target.value);
+}
+
+const handelRegister=e=>{
+  e.preventDefault()
+  console.log(email ,password);
+  if (password.length < 6) {
+    setError('Password Must be at least 6 characters long.')
+    return;
+  }
+  if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+    setError('Password Must contain 2 upper case');
+    return;
+  }
+  
+  if (isLogin) {
+    processLogin(email, password);
+  }
+  else {
+    registerNewUser(email, password);
+  }
+ 
+  
+}
+
+const userInputApply=e=>{
+  setisLogin(e.target.checked);
+}
+
+const processLogin =()=>{
+  createUserWithEmailAndPassword (auth , email,password)
+  .then(result =>{
+    const user = result.user;
+    console.log(user);
+    setError('')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    setError(errorCode)
+    // ..
+  });
+}
+
+const registerNewUser=(email ,password)=>{
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((result)=>{
+    const user = result.user;
+    console.log(user);
+    setError("")
+    EmailVerification()
+  })
+  .catch(error =>{
+    setError(error.massage)
+  })
+}
+
+const EmailVerification =() =>{
+  sendEmailVerification(auth.currentUser)
+  .then( result => {
+    console.log(result);
+  });
+}
+const hendelResetPassword =()=>{
+  sendPasswordResetEmail(auth ,email)
+  .then(result =>{
+    console.log("success");
+  })
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+    <form onSubmit={handelRegister}>
+      <h1 className="text-primary text-center">Please {isLogin ? 'Login' : 'Register'} </h1>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input onBlur={emailhendelar} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+    <input onBlur={passwordhendel} type="password" className="form-control" id="exampleInputPassword1" />
+  </div>
+
+  <div className="mb-3 form-check">
+    <input type="checkbox" onChange={userInputApply} className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Already Registered ?</label>
+  </div>
+
+  <div className="row mb-3 text-denger">
+    {error}
+  </div>
+ 
+  <button type="submit" className="btn btn-primary">
+  {isLogin ? 'Login' : 'Register'}
+    </button>
+
+    <button type="button" onClick={hendelResetPassword}>Reset Password</button>
+</form>
     </div>
+      <br />
+      <br />
+      <br />
+            <div>--------------------------------------</div>
+      <br />
+      <br />
+      <button onClick={googleLoginClick}>Google Sing In</button>
+     
+    </>
   );
 }
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8_vWttiIpuiWkz7je4b2PPIXUAxXmr_g",
+    authDomain: "email-password-modul-57.firebaseapp.com",
+    projectId: "email-password-modul-57",
+    storageBucket: "email-password-modul-57.appspot.com",
+    messagingSenderId: "23146395831",
+    appId: "1:23146395831:web:f8cb959e3f7039553bf532"
+  };
+
+  export default firebaseConfig;
